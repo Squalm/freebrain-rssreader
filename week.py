1b1dc8ee4097b5f11c47376b1a28db15064f6679
Chirp <43246103+Squalm@users.noreply.github.com>
2023-07-17 12:33:17 +0000

    run this each week

---
+++ week.py
@@ -0,0 +1,10 @@
+from main import * # import functions from main
+
+URI = "neo4j://localhost:7687"
+# Grab admin secret
+secret = getSecret()
+# Connect to db
+db = Graph(URI, ("neo4j", secret))
+
+db.query(f"MATCH (n) SET n.{db.datestr} = 0")
+db.query(f"MATCH ()-[l]-() SET n.{db.datestr} = 0")
